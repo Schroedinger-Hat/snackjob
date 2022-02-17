0d38436139118a7a25154f4fa006f3c99d82cd12
Wabri <gabriele.puliti@pm.me>
2022-02-17 13:08:29 +0100

    feature: add more complexity and maybe some crap

--- app.py
+++ app.py
@@ -5,17 +5,26 @@ app = Flask(__name__)
 
 d = {
     'experience': [ 'junior', 'mid', 'intermediate', 'senior', 'expert' ],
-    'name': [ 'UX', 'UI', 'Developer', 'Devops' ],
-    'tech': [ 'java', 'angular', 'ansible', 'python' ]
+    'name': [ 'UX', 'UI', 'Developer', 'Devops', ''],
+    'tech': [ 'Machine Learning', 'UI', 'Developer', 'Devops' ]
 }
-random.choice(list(d.values()))
 
 @app.route('/way', methods=['GET'])
 def way():
     return get_random_job()
 
 def get_random_job():
-   experience = random.choice(list(d['experience']))
-   name = random.choice(list(d['name']))
-   tech = random.choice(list(d['tech']))
-   return experience + ' ' + name + ' ' + tech
+    random.seed()
+    tmp = ""
+    for i in range(random.randint(1, 4)):
+        random.seed()
+        tmp2 = random.choice(d['name'])
+        while tmp2 in tmp:
+            random.seed()
+            tmp2 = random.choice(d['name'])
+        tmp3 = random.choice(d['tech'])
+        while tmp3 in tmp:
+            random.seed()
+            tmp2 = random.choice(d['tech'])
+        tmp += tmp3 + ' '
+    return random.choice(d['experience']) + ' ' + tmp
