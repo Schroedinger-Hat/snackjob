3e321804eb4f8b4ef40be7ba383c9488d232d366
Wabri <gabriele.puliti@pm.me>
2022-02-17 17:05:11 +0100

    feature: update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ experience_list = {
 position_list = {
     'Digital': 40,
     'Tech lead': 50,
+    'Head of': 50,
     'Devops': 60,
     'Developer': 60,
     'Frontend Developer': 60,
@@ -25,7 +26,6 @@ position_list = {
     'Manager': 70,
     'CEO': 80,
     'CTO': 80,
-    'Head of': 80,
     'Business Solution': 80,
     'Manager': 80,
     'Staff Engineer': 80,
@@ -55,49 +55,50 @@ reinforcement_list = {
 
 @app.route('/api/v1/snackjob', methods=['POST'])
 def snackjob():
-    job = request.form.get('job')
-    print(job)
+    job = request.form.get('job') or ""
+    power = request.form.get('power') or 100
     return {
-            "snack_job": get_snack_job(len(job.split())),
+            "snack_job": get_snack_job(len(job.split()), float(power)),
         }
 
-def get_snack_job(section_number=3):
+def get_snack_job(section_number=3, power=100.0):
 
     total_power = 0
     snack_job = ""
 
     # exp
     random.seed()
-    experience = random.choice(list(experience_list.keys()))
-    total_power += experience_list[experience]
-    snack_job += experience
+    if bool(random.getrandbits(1)):
+        experience = random.choice(list(experience_list.keys()))
+        total_power += experience_list[experience]
+        snack_job += experience + ' '
 
     # position
-    while total_power < 50:
+    while total_power < 0.5*power:
         random.seed()
         position = random.choice(list(position_list.keys()))
         total_power += position_list[position]
-        snack_job += ' ' + position
+        snack_job += position + ' '
     if len(snack_job.split()) >= section_number:
-        return snack_job
+        return snack_job.rstrip()
 
     # tech
-    while total_power < 90:
+    while total_power < 0.9*power:
         random.seed()
         tech = random.choice(list(tech_list.keys()))
         total_power += tech_list[tech]
-        snack_job += ' ' + tech
+        snack_job += tech + ' '
     if len(snack_job.split()) >= section_number:
-        return snack_job
+        return snack_job.rstrip()
 
     # reinforcementes
-    while total_power <= 100:
+    if total_power <= power:
         random.seed()
         reinforcement = random.choice(list(reinforcement_list.keys()))
         total_power += reinforcement_list[reinforcement]
-        snack_job += ' ' + reinforcement
+        snack_job += reinforcement + ' '
     if len(snack_job.split()) >= section_number:
-        return snack_job
+        return snack_job.rstrip()
 
-    return snack_job
+    return snack_job.rstrip()
 
