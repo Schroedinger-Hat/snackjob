b2e0802b930747bfd4f4dbda4ab765c7b54febef
Miki <miki.lombi@gmail.com>
2022-08-22 15:32:30 +0200

    fix: unsecure request from google.font

--- snackjob-frontend/pages/_document.js
+++ snackjob-frontend/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css' />
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css' />
         </Head>
         <body>
           <Main />
